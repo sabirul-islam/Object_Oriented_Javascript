7dbc2868e636c3477e75ad2d0e13d16559cf2a0e
sabirul-islam <sabirul.shimul@gmail.com>
2021-03-16 09:47:19 +0600

    class constructor added

---
+++ Stack_Learner/class_constructor.js
@@ -0,0 +1,12 @@
+class Person{
+    constructor(name, email){
+        this.name = name
+        this.email = email
+    }
+}
+
+const p1 = new Person("sabirul", "sabirul.shimul@gmail.com")
+const p2 = new Person("shimul", "aborzona@gmail.com")
+
+console.log(p1);
+console.log(p2);
